2921dcd7da58a2bfd98c74a5d120016b333afb05
Elvisley Souza <elvis.s.p90@gmail.com>
2017-01-06 16:57:08 -0200

    Implementando listagem

--- app/src/main/java/com/example/android/miwok/NumberActivity.java
+++ app/src/main/java/com/example/android/miwok/NumberActivity.java
@@ -2,6 +2,14 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -9,5 +17,24 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+        words.add(new Word("one","lutti"));
+//        words.add("two");
+//        words.add("three");
+//        words.add("four");
+//        words.add("five");
+//        words.add("six");
+//        words.add("seven");
+//        words.add("eight");
+//        words.add("nine");
+//        words.add("ten");
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+
+        ListView listView = (ListView) findViewById(R.id.activity_root_view_number);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,44 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Solution Process Web on 06/01/2017.
+ */
+
+public class WordAdapter  extends ArrayAdapter<Word>{
+
+    public WordAdapter(Activity context, ArrayList<Word> words){
+        super(context , 0 , words);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        Word wordgetitem = getItem(position);
+
+        TextView miwok_text_view = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwok_text_view.setText(wordgetitem.getMiwokTranslation());
+
+        TextView dafault_text_view = (TextView) listItemView.findViewById(R.id.dafault_text_view);
+        dafault_text_view.setText(wordgetitem.getDefaultTranslation());
+
+
+        return listItemView;
+    }
+}
